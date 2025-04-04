68953ea1cc90ce34e5e7907b5f57b5eeb0ddb34b
milanprajapati571 <milanprajapati571@gmail.com>
2025-04-05 03:07:23 +0530

    C=Initial push

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React, {  useEffect } from 'react'
+import { Routes,Route, Navigate} from 'react-router-dom'
+import { Loader } from 'lucide-react'
+import { Toaster } from 'react-hot-toast'
+import './App.css'
+
+function App() {
+  
+  return (
+   <div>Hi</div>
+  )
+}
+
+export default App
